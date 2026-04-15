4ac47e16f2d42736547b9672c1bc8386c0722916
alinarogova123-arch <alinarogova123@gmail.com>
2026-04-15 16:35:18 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -2,12 +2,16 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 import json
+import os
+
+
+os.makedirs("pages", exist_ok=True)
 
 
 with open("meta_data.json", "r", encoding='utf-8') as my_file:
     books = json.loads(my_file.read())
 
-books_of_two = list(chunked(books, 2))
+books_for_pages = list(chunked(books, 20))
 
 env = Environment(
     loader=FileSystemLoader('.'),
@@ -16,11 +20,13 @@ env = Environment(
 
 def on_reload():
     template = env.get_template('template.html')
-    rendered_page = template.render(
-    books_of_two=books_of_two,
-    )
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(rendered_page)
+    for num_page, books_for_page in enumerate(books_for_pages, 1):
+        books_of_two = list(chunked(books_for_page, 2))
+        rendered_page = template.render(
+        books_of_two=books_of_two,
+        )
+        with open(f'pages/index{num_page}.html', 'w', encoding="utf8") as file:
+            file.write(rendered_page)
 
 on_reload()
 
@@ -28,4 +34,4 @@ server = Server()
 
 server.watch('template.html', on_reload)
 
-server.serve(root='.')
+server.serve(root='pages')
