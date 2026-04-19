7897acf11b88f2560058c5a7f7a02d0dd942f9ef
alinarogova123-arch <alinarogova123@gmail.com>
2026-04-19 11:43:54 +0300

    move img and text in media

--- render_website.py
+++ render_website.py
@@ -6,14 +6,19 @@ import os
 import math
 
 
-def on_reload():
-    template = env.get_template('template.html')
+def get_books():
+    with open("meta_data.json", "r", encoding='utf-8') as my_file:
+        books = json.load(my_file)
+    return books
+
+
+def on_reload(books_for_pages, template, page_count):
     for page_number, books_for_page in enumerate(books_for_pages, 1):
         books_of_two = list(chunked(books_for_page, 2))
         rendered_page = template.render(
         books_of_two=books_of_two,
         page_number=page_number,
-        page_count=math.ceil(len(books) / 20)
+        page_count=page_count
         )
         with open(f'pages/index{page_number}.html', 'w', encoding="utf8") as file:
             file.write(rendered_page)
@@ -21,15 +26,16 @@ def on_reload():
 
 if __name__ == '__main__':
     os.makedirs("pages", exist_ok=True)
-    with open("meta_data.json", "r", encoding='utf-8') as my_file:
-        books = json.loads(my_file)
+    books = get_books()
     books_for_pages = list(chunked(books, 20))
+    page_count=math.ceil(len(books) / 20)
     env = Environment(
     loader=FileSystemLoader('.'),
     autoescape=select_autoescape(['html', 'xml'])
     )
-    on_reload()
+    template = env.get_template('template.html')
+    on_reload(books_for_pages, template, page_count)
     server = Server()
-    server.watch('template.html', on_reload)
+    server.watch('template.html', on_reload(books_for_pages, template, page_count))
     server.serve(root='.')
 
